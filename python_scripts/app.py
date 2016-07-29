3784e5f2bcc5ac1f33c6c5b4454e5c3ada83e693
David Steinberg <david@resium.com>
2016-07-29 15:39:26 -0700

    Improved hello world to include JSON response and request

--- python_scripts/app.py
+++ python_scripts/app.py
@@ -1,13 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-@app.route("/bye")
+@app.route("/bye", methods=["POST"])
 def bye():
-    return "bye World!"
+    name = flask.request.get_json()['name']
+    response = {}
+    response['message'] = "bye {}!".format(name)
+    return flask.jsonify(response)
 
 if __name__ == "__main__":
+    app.debug = True  # helps us figure out if something went wrong
     app.run()
